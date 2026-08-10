ca27d21f23adece55792fc7bec6a78d3313dc91d
Bhawana783 <kbhawana1411@gmail.com>
2026-08-10 21:20:21 +0530

    chore: remove frontend and backend folders

--- backend/src/index.js
+++
@@ -1,7 +0,0 @@
-import app from './app.js';
-
-const port = Number(process.env.PORT ?? 5050);
-
-app.listen(port, () => {
-  console.log(`OutageIQ backend running on http://localhost:${port}`);
-});
